250266f48aac00987216cc9b5e7f294d89c67477
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-03-30 12:07:40 +0600

    store,reducer,dispatch,getState done

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ function counterReducer(state = initialState,action) {
     switch (action.type){
         case 'increment':
             return { value: state.value + 1 }
+        case 'decrement':
+            return { value: state.value - 1 }
         default:
             return state
     }
@@ -17,4 +19,9 @@ function counterReducer(state = initialState,action) {
 
 let store = createStore(counterReducer);
 
-console.log(store.getState())
+console.log('store', store);
+console.log('store previous state', store.getState());
+store.dispatch({type: 'increment'})
+console.log('store previous state after incrementing',store.getState() );
+store.dispatch({type: 'decrement'})
+console.log('store previous state after decrementing', store.getState());
