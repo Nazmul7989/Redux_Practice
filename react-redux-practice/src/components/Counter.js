53ae95d1a972c66eccc71a28ceb6efd4a4cb0962
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-03-30 20:10:13 +0600

    action dispatch by using useDispatch hook

--- react-redux-practice/src/components/Counter.js
+++ react-redux-practice/src/components/Counter.js
@@ -1,13 +1,20 @@
 import React, {Fragment} from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 const Counter = () => {
 
     const counter = useSelector((state) => state.counterReducer.value)
+    const dispatch = useDispatch();
 
     return (
         <Fragment>
-            <h4>Counter Value: { counter}</h4>
+            <h4>Counter App</h4>
+            <div>
+                <button onClick={()=> dispatch({type: 'increment'})}>+</button>
+                <span style={{ padding: '10px'}}>{ counter}</span>
+                <button onClick={()=> dispatch({type: 'decrement'})}>-</button>
+            </div>
+
         </Fragment>
     );
 };
--- react-redux-practice/src/components/Task.js
+++ react-redux-practice/src/components/Task.js
@@ -1,19 +1,40 @@
 import React, {Fragment} from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 const Task = () => {
 
-    const tasks = useSelector((state) => state.taskReducer.task)
+    const {formInput,task} = useSelector((state) => state.taskReducer)
+    const dispatch = useDispatch();
 
-    const taskData = tasks.map((task,index)=>{
+    const taskData = task.map((task,index)=>{
         return <ul key={index}>
             <li>{ task.title }</li>
         </ul>
     })
 
+    const onChangeHandler = (value)=>{
+
+        dispatch({type: 'changeInput', payload: value})
+    }
+
+    const onClickHandler = (e) =>{
+        e.preventDefault();
+
+        let titleData = {
+            id: new Date().getMilliseconds(),
+            title: formInput
+        }
+
+        dispatch({type: 'changeTask', payload: titleData})
+    }
 
     return (
         <Fragment>
+
+            <form>
+                <input value={formInput} onChange={(e)=>onChangeHandler(e.target.value)} type="text"/>
+                <input type="submit" onClick={onClickHandler} value="Save"/>
+            </form>
 
             { taskData}
 
